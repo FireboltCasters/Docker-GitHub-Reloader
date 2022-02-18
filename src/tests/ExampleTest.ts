f71b6e78d7372eca611ed979f695db60724e0727
Lint Action <lint-action@samuelmeuli.com>
2022-02-18 10:32:32 +0000

    Fix code style issues with Prettier

--- src/tests/ExampleTest.ts
+++ src/tests/ExampleTest.ts
@@ -2,6 +2,6 @@ import Example from '../Example';
 
 test('Example Test', async () => {
   let result = Example.multiply(3, 4);
-  console.log(result)
+  console.log(result);
   expect(result).toBe(12);
 });
