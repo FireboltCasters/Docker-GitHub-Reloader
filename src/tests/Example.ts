b8be120d7a75ffd026495dea3bfd5e21ce99bdd1
Lint Action <lint-action@samuelmeuli.com>
2022-02-18 10:16:27 +0000

    Fix code style issues with Prettier

--- src/tests/Example.ts
+++ src/tests/Example.ts
@@ -1,5 +1,5 @@
-export default class Example{
-    public static multiply(a: any, b: any){
-        return a*b;
-    }
+export default class Example {
+  public static multiply(a: any, b: any) {
+    return a * b;
+  }
 }
